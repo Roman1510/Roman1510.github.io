18c94c0bfb7f5e46cc212fa0a422483c2f7e5ec4
Roman1510 <romanvinnick@gmail.com>
2023-08-10 01:16:03 +0200

    dark mode colors done

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -69,7 +69,7 @@ const Navbar: React.FC = () => {
                   {projects.map((item) => (
                     <div
                       key={item.name}
-                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-indigo-300"
+                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-slate-800"
                     >
                       <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-800 group-hover:bg-slate-700">
                         <div className="h-6 w-6 text-gray-600 group-hover:text-indigo-600">
@@ -82,12 +82,12 @@ const Navbar: React.FC = () => {
                       <div className="flex-auto">
                         <a
                           href={item.href}
-                          className="block font-semibold text-gray-900 "
+                          className="block font-semibold text-gray-300 "
                         >
                           {item.name}
                           <span className="absolute inset-0" />
                         </a>
-                        <p className="mt-1 text-gray-900">{item.description}</p>
+                        <p className="mt-1 text-gray-400">{item.description}</p>
                       </div>
                     </div>
                   ))}
