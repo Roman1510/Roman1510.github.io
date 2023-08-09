7113565c1fbe87485ddeae7dde160a99421747b6
Roman1510 <romanvinnick@gmail.com>
2023-08-10 01:13:17 +0200

    changing the colors

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -64,16 +64,17 @@ const Navbar: React.FC = () => {
               leaveFrom="opacity-100 translate-y-0"
               leaveTo="opacity-0 translate-y-1"
             >
-              <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
+              <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-violet-800 shadow-lg ring-1 ring-gray-900/5">
                 <div className="p-4">
                   {projects.map((item) => (
                     <div
                       key={item.name}
-                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
+                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-indigo-300"
                     >
-                      <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                      <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-800 group-hover:bg-slate-700">
                         <div className="h-6 w-6 text-gray-600 group-hover:text-indigo-600">
                           <item.icon
+                            className="text-slate-300"
                             aria-hidden="true"
                           />
                         </div>
@@ -86,7 +87,7 @@ const Navbar: React.FC = () => {
                           {item.name}
                           <span className="absolute inset-0" />
                         </a>
-                        <p className="mt-1 text-gray-600">{item.description}</p>
+                        <p className="mt-1 text-gray-900">{item.description}</p>
                       </div>
                     </div>
                   ))}
