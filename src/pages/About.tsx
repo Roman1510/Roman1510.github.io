5a966c4e86624edfbc1f8f0ee72482fc4d5a1458
Roman1510 <romanvinnick@gmail.com>
2023-08-11 15:16:54 +0200

    adding margin

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -40,7 +40,7 @@ const About: React.FC = () => {
 
   return (
     <div className="hero-section bg-indigo-900 bg-opacity-50 text-white h-screen flex flex-col justify-center items-center absolute inset-0">
-      <div className="max-w-screen-lg mx-auto px-4">
+      <div className="container-margin max-w-screen-lg mx-auto px-4">
         <p className="text-white text-lg text-center">
           'Sup! so this is what I actually do on a daily basis:
         </p>
