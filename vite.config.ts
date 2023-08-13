d11adf904367da9766196f4f4749eb6a73a68297
Roman1510 <romanvinnick@gmail.com>
2023-08-13 14:35:06 +0200

    new changes trying to fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://Roman1510.github.io/github.io',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
