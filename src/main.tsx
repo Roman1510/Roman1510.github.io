6e65c8fee4accf831501d0dc413f36776bbe7eab
Vinnick <romanvinnick@gmail.com>
2023-08-13 17:26:58 +0200

    changed vite config

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <HashRouter basename={'/github.io'}>
+    <HashRouter >
         <App />
     </HashRouter>,
 )
