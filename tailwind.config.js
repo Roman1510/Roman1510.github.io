ffecb2996137377d1f937334e958e2f43ee2f518
Vinnick <romanvinnick@gmail.com>
2023-08-13 17:43:21 +0200

    small fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{js,jsx,ts,tsx}',],
+  content: ["./src/**/*.{html,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
