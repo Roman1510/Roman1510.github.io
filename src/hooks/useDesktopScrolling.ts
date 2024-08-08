f32c601bbd110984959c5fb9ee35969996d58847
Roman1510 <romanvinnick@gmail.com>
2024-08-08 12:20:55 +0200

    adding arrows handling

--- src/hooks/useDesktopScrolling.ts
+++ src/hooks/useDesktopScrolling.ts
@@ -29,8 +29,8 @@ export const useDesktopScrolling = () => {
         scrollTrigger: {
           trigger: desktopContainer,
           pin: true,
-          scrub: 0.7,
-          anticipatePin: 1,
+          scrub: 0.85,
+          anticipatePin: 0.01,
           snap: {
             snapTo: 1 / (sections.length - 1),
             duration: { min: 0.15, max: 0.3 },
@@ -39,6 +39,34 @@ export const useDesktopScrolling = () => {
           end: () => '+=' + desktopContainer.offsetWidth,
         },
       })
+
+      const handleKeydown = (event: KeyboardEvent) => {
+        const isHorizontalScroll = ['ArrowLeft', 'ArrowRight'].includes(
+          event.key
+        )
+
+        if (isHorizontalScroll) {
+          event.preventDefault()
+        }
+
+        if (event.key === 'ArrowRight' || event.key === 'ArrowDown') {
+          gsap.to(window, {
+            scrollTo: { y: '+=' + window.innerWidth / 8, autoKill: false },
+            duration: 0.5,
+          })
+        } else if (event.key === 'ArrowLeft' || event.key === 'ArrowUp') {
+          gsap.to(window, {
+            scrollTo: { y: '-=' + window.innerWidth / 8, autoKill: false },
+            duration: 0.5,
+          })
+        }
+      }
+
+      window.addEventListener('keydown', handleKeydown)
+
+      return () => {
+        window.removeEventListener('keydown', handleKeydown)
+      }
     } else {
       ScrollTrigger.getAll().forEach((trigger) => trigger.kill())
     }
