dd4e4ec52ac81b87f476a92b1f1b0ad2a6aa3bc0
Roman1510 <romanvinnick@gmail.com>
2024-08-14 23:38:42 +0200

    use select adding

--- src/components/content/Second.tsx
+++ src/components/content/Second.tsx
@@ -2,7 +2,7 @@ export const Second = () => {
   return (
     <div className="section" style={{ backgroundColor: '#9b72cf' }}>
       <div className="title" style={{ padding: '50px' }}>
-        <p style={{ padding: '50px' }}>Tha</p>
+        <p style={{ padding: '50px', userSelect: 'none' }}>Tha</p>
       </div>
     </div>
   )
