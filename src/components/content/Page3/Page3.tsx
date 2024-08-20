e0fa5648270c8557a8d7ddc3dd9a0491b8df63bd
Roman1510 <romanvinnick@gmail.com>
2024-08-20 23:07:32 +0200

    adding spritesheets + some grid setup (helper for debugging)

--- src/components/content/Page3/Page3.tsx
+++ src/components/content/Page3/Page3.tsx
@@ -3,8 +3,10 @@ import { PixiGrid } from './PixiGrid'
 export const Page3 = () => {
   return (
     <div className="section" style={{ backgroundColor: '#9fffcb' }}>
-      <div className="title" style={{ padding: '50px' }}>
+      {/* <div className="title" style={{ padding: '50px' }}>
         <p style={{ padding: '50px' }}>Best</p>
+      </div> */}
+      <div>
         <PixiGrid />
       </div>
     </div>
--- src/components/content/Page3/PixiGrid.tsx
+++ src/components/content/Page3/PixiGrid.tsx
@@ -1,15 +1,39 @@
-import { Container, Sprite, Stage } from '@pixi/react'
-// import { BlurFilter, TextStyle } from 'pixi.js'
-// import { useMemo } from 'react'
+import { Container, Graphics, Stage } from '@pixi/react'
+import { Graphics as GraphicsImpl } from 'pixi.js'
 
 export const PixiGrid = () => {
-  // const blurFilter = useMemo(() => new BlurFilter(0.3), [])
-  const bunnyUrl = 'https://pixijs.io/pixi-react/img/bunny.png'
+  const TILE_SIZE = 32
+  const COLS = 48
+  const ROWS = 31
+  const GAME_WIDTH = TILE_SIZE * COLS
+  const GAME_HEIGHT = TILE_SIZE * ROWS
+
+  const drawGrid = (g: GraphicsImpl) => {
+    g.clear()
+    g.lineStyle(1, 0xcccccc, 1)
+
+    for (let x = 0; x <= COLS; x++) {
+      g.moveTo(x * TILE_SIZE, 0)
+      g.lineTo(x * TILE_SIZE, GAME_HEIGHT)
+    }
+
+    for (let y = 0; y <= ROWS; y++) {
+      g.moveTo(0, y * TILE_SIZE)
+      g.lineTo(GAME_WIDTH, y * TILE_SIZE)
+    }
+  }
 
   return (
-    <Stage width={1200} height={900} options={{ background: 0x1099bb }}>
-      <Container x={200} y={200}>
-        <Sprite image={bunnyUrl} x={0} y={0} />
+    <Stage
+      width={GAME_WIDTH}
+      height={GAME_HEIGHT}
+      options={{
+        backgroundAlpha: 0,
+        antialias: true,
+      }}
+    >
+      <Container>
+        <Graphics draw={drawGrid} />
       </Container>
     </Stage>
   )
