809aaa628ffbab947e5eec7e316b0885e1fb3685
Roman1510 <romanvinnick@gmail.com>
2024-09-08 16:18:30 +0200

    minor change

--- src/components/content/Page3/Page3.tsx
+++ src/components/content/Page3/Page3.tsx
@@ -3,12 +3,12 @@ import { PixiGrid } from './Experience/PixiGrid'
 export const Page3 = () => {
   return (
     <div className="section" style={{ backgroundColor: '#9fffcb' }}>
-      <p className="title" style={{ padding: '50px' }}>
-        [WASD]
-      </p>
       <div>
         <PixiGrid />
       </div>
+      <p className="title" style={{ padding: '50px', width: '25%' }}>
+        [WASD] to control
+      </p>
     </div>
   )
 }
