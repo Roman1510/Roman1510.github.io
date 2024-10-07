60cea38ca4adbd2129d57a570fe49d33aa3f480e
Roman1510 <romanvinnick@gmail.com>
2024-10-07 08:18:15 +0200

    chaning the pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
-import './index.css'
-import { useDeviceType } from './hooks/useDeviceType'
-import { DesktopWrapper } from './components/content/DesktopWrapper'
-import { MobileWrapper } from './components/content/MobileWrapper'
-import { useAnimatedFavicon } from './hooks/useAnimatedFavicon'
-import { Page1 } from './components/content/Page1/Page1'
-import { Page2 } from './components/content/Page2/Page2'
-import { Page3 } from './components/content/Page3/Page3'
-import { Page4 } from './components/content/Page4/Page4'
-import { Page5 } from './components/content/Page5/Page5'
-import { ResizePlaceholder } from './components/ResizePlaceholder'
+import './index.css';
+import { useDeviceType } from './hooks/useDeviceType';
+import { DesktopWrapper } from './components/content/DesktopWrapper';
+import { MobileWrapper } from './components/content/MobileWrapper';
+import { useAnimatedFavicon } from './hooks/useAnimatedFavicon';
+import { Page1 } from './components/content/Page1/Page1';
+import { Page2 } from './components/content/Page2/Page2';
+import { Page3 } from './components/content/Page3/Page3';
+import { Page4 } from './components/content/Page4/Page4';
+import { Page5 } from './components/content/Page5/Page5';
+import { ResizePlaceholder } from './components/ResizePlaceholder';
 
 const App = () => {
-  useAnimatedFavicon()
-  const deviceType = useDeviceType()
+  useAnimatedFavicon();
+  const deviceType = useDeviceType();
 
   const content = (
     <>
+      <Page3 />
       <Page1 />
       <Page2 />
-      <Page3 />
       <Page4 />
       <Page5 />
     </>
-  )
+  );
 
   return deviceType === 'desktop' ? (
     <DesktopWrapper>
@@ -31,7 +31,7 @@ const App = () => {
     </DesktopWrapper>
   ) : (
     <MobileWrapper>{content}</MobileWrapper>
-  )
-}
+  );
+};
 
-export default App
+export default App;
