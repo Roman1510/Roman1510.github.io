ea2fda85cfe85dfab15b3f9be277dd0a93dc3762
Roman1510 <romanvinnick@gmail.com>
2024-10-20 22:23:14 +0200

    adding boilerplate to the page 4

--- src/components/content/Page4/Experience.tsx
+++ src/components/content/Page4/Experience.tsx
@@ -1,12 +1,21 @@
-import { Box, OrbitControls } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Box } from '@react-three/drei';
+import { Suspense } from 'react';
 
 const Experience = () => {
   return (
-    <div>
-      <Canvas>
-        <OrbitControls />
-        <Box />
+    <div style={{ backgroundColor: '#808080' }}> {/* Grey Background */}
+      <Canvas shadows>
+        <Suspense fallback={null}>
+          <OrbitControls />
+
+          <Box position={[0, 0, 0]} args={[1, 1, 1]} castShadow receiveShadow>
+            <meshStandardMaterial attach="material" color="orange" />
+          </Box>
+
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[5, 5, 5]} intensity={1} castShadow />
+        </Suspense>
       </Canvas>
     </div>
   );
--- src/components/content/Page4/Page4.tsx
+++ src/components/content/Page4/Page4.tsx
@@ -2,11 +2,38 @@ import Experience from "./Experience"
 
 export const Page4 = () => {
   return (
-    <div className="section" style={{ backgroundColor: '#c8b1e4' }}>
-      <div className="title" style={{ padding: '50px' }}>
-        <div><Experience /></div>
+    <div className="section" style={{ backgroundColor: '#c8b1e4', height: '100vh' }}>
+      <div className="title" >
         <p style={{ padding: '50px' }}>This page is...</p>
+      </div>
+      <div style={styles.wrapper}>
+        <div style={{ height: '50%' }}>
+          <Experience />
+        </div>
       </div>
     </div>
   )
 }
+
+
+const styles = {
+  wrapper: {
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    bottom: 0,
+  } as React.CSSProperties,
+
+  headerText: {
+    position: 'absolute',
+    top: '10%',
+    left: '50%',
+    transform: 'translateX(-50%)',
+    fontSize: '64px',
+    fontFamily: "'YourFontName', sans-serif",
+    color: 'white',
+    textAlign: 'center',
+    userSelect: 'none',
+    zIndex: 11,
+  } as React.CSSProperties,
+};
