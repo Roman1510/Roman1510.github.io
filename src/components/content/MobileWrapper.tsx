ec08fd7b0bfdcb8bb76abf1680427fc00a9e8a32
Roman1510 <romanvinnick@gmail.com>
2024-11-03 14:08:39 +0100

    getting rid of redundant code

--- src/components/content/MobileWrapper.tsx
+++ src/components/content/MobileWrapper.tsx
@@ -1,25 +1,5 @@
-import { PropsWithChildren, useState } from 'react';
-import { useShake } from '@/hooks/useShake';
+import { PropsWithChildren } from 'react'
 
 export const MobileWrapper = ({ children }: PropsWithChildren) => {
-  const [isMatrixFont, setIsMatrixFont] = useState(true);
-
-  const handleShake = () => {
-    setIsMatrixFont((prev) => !prev);
-  };
-
-  useShake(handleShake);
-
-  return (
-    <div
-      className="mobile-container"
-      style={{
-        fontFamily: isMatrixFont
-          ? 'MatrixCode, sans-serif'
-          : 'Comic Neue, sans-serif',
-      }}
-    >
-      {children}
-    </div>
-  );
-};
+  return <div className="mobile-container">{children}</div>
+}
